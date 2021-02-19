ed7a193f6fb853000ec83f773d27e70eca819440
ARealWant <abdu.ibrahim@outlook.de>
2021-02-19 18:39:59 +0100

    Possibilty to choose between the Versions

---
+++ main.py
@@ -0,0 +1,11 @@
+﻿import os
+print("Choose between the Normal and Rigged version [n/r]:")
+
+answer = input()
+
+if answer.lower() == "r":
+    print("Ok! Starting the Rigged Version...\n")
+    os.system("riggedScissorsRockPaper.py")
+else:
+    print("Ok! Starting the Normal Version...\n")
+    os.system("ScissorsRockPaper.py")
